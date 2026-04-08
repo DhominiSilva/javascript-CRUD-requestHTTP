5c13cd8f421e8bf0a700f6bad25288778fb7289f
Dhomini Silva <dhominisilva1@gmail.com>
2026-04-07 22:22:08 -0300

    feat: conectando o frontend com a API fake

---
+++ backend/js/api.js
@@ -0,0 +1,15 @@
+const api = {
+    async buscarPensamentos(){ // async espera a resposta da API sem travar o programa
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos'); // await espera a resposta da API sem travar o programa
+            return await response.json(); // converte a resposta da API para um objeto JS
+            
+        }
+        catch {
+            alert('Não foi possível carregar os pensamentos');
+            throw error;
+        }
+    }
+}
+
+export default api; // exporta o objeto api para ser usado em outros arquivos do projeto
