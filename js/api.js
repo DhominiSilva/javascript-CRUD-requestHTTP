df2e7d9eb85925e3b24d3580468f794b108289b9
Dhomini Silva <dhominisilva1@gmail.com>
2026-06-22 23:12:31 -0300

    feat: implementando método de alteração de dados com o PUT

--- js/api.js
+++ js/api.js
@@ -26,6 +26,35 @@ const api = {
             alert('Não foi possível carregar os pensamentos');
             throw error;
         }
+    },
+
+    async buscarPensamentoById(id){ // async espera a resposta da API sem travar o programa
+        // método get para buscar os pensamentos da API, por padrão o fetch usa o método GET, então não é necessário especificar
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`); // await espera a resposta da API sem travar o programa
+            return await response.json(); // converte a resposta da API para um objeto JS
+        }
+        catch (error) {
+            alert('Erro ao salvar pensamento');
+            throw error;
+        }
+    },
+
+    async editarPensamento(pensamento){ // async espera a resposta da API sem travar o programa
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: 'PUT', // método HTTP para enviar dados
+                headers: {
+                    "Content-Type": "application/json" // tipo de conteúdo para enviar os dados em formato JSON
+                },
+                body: JSON.stringify(pensamento) // converte o objeto pensamento para uma string JSON para enviar para a API
+            });
+            return await response.json(); 
+        }
+        catch (error) {
+            alert('Erro ao editar pensamento');
+            throw error;
+        }
     }
 }
 
